c83ebbe6d150517ed76935be489e8316b35a371f
Youssef <youssef1995elam@gmail.com>
2023-09-13 12:52:27 +0200

    Fix project card

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,7 +4,6 @@ import React, { useRef } from 'react'
 import { useScroll, motion, useTransform } from 'framer-motion'
 import { projectsData } from '@/lib/data'
 import { useLanguage } from '@/hooks/useLanguage'
-import { VscGithub } from "react-icons/vsc";
 import Image from 'next/image'
 
 type ProjectProps = (typeof projectsData)[number]
@@ -15,7 +14,6 @@ export default function Project({
     desc_de,
     tags,
     gitUrl,
-    url,
     imageUrl
 }: ProjectProps) {
 
@@ -33,17 +31,13 @@ export default function Project({
 
     return (
         <motion.div ref={ref} style={{ scale: scaleProgress, opacity: opacityProgress }} className="group mb-3 sm:mb-8 last:mb-0">
-            <a href={url} target="_blank">
+            <a href={gitUrl} target="_blank">
                 <section 
                     className="bg-gray-100 max-w-[42rem] border rounded-lg border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] 
                     sm:group-even:pl-8 hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20 dark:text-white">
                     <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-                        <h3 className="text-2xl font-semibold flex gap-2">
-                            {title} 
-
-                            <a href={gitUrl} target="_blank" >
-                                <VscGithub className="inline mb-1"/>
-                            </a>
+                        <h3 className="text-2xl font-semibold">
+                            {title}
                         </h3>
                         <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{language === "de" ? desc_de : description}</p>
                         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
